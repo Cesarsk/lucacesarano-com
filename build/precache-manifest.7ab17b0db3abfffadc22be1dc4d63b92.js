5d12500bbf1f4dca3441c53ccee175145ecec90d
Cesarsk <luca.cesarano1@gmail.com>
2019-05-05 18:04:25 +0200

    quickfix

---
+++ build/precache-manifest.7ab17b0db3abfffadc22be1dc4d63b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14389f1ccfddd67cf1596422c3bb26e6",
+    "url": "/static/media/profile-pic.14389f1c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a345e1f80a40ba895d6",
+    "url": "/static/js/main.4a345e1f.chunk.js"
+  },
+  {
+    "revision": "953705f5b92d4758ae9f",
+    "url": "/static/js/1.953705f5.chunk.js"
+  },
+  {
+    "revision": "4a345e1f80a40ba895d6",
+    "url": "/static/css/main.3fba717b.chunk.css"
+  },
+  {
+    "revision": "baa4403e39c460d33d192a503b9705ee",
+    "url": "/index.html"
+  }
+];
