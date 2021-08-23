c4b85e9bde63b13187f7fc6c87fd5e50e1c14449
lu.cesarano <9QbNmbGjQp5UED9>
2021-08-23 22:56:11 +0200

    CI/CD pipeline - master

--- src/components/Work.js
+++ src/components/Work.js
@@ -39,7 +39,7 @@ function RenderContainer() {
 
         <p>Say It! App</p>
         <div className="Work-list">
-          <a className="Work-link" href="https://play.google.com/store/apps/details?id=com.cesarsk.say_it"> Say It! For Android(~ 30k users, 4.7/5.0 ★) 🇬🇧</a>
+          <a className="Work-link" href="https://play.google.com/store/apps/details?id=com.cesarsk.say_it"> Say It! For Android(~ 30k users, 4.7/5.0 <span role="img">★</span>) 🇬🇧</a>
         </div>
         <br />
 
