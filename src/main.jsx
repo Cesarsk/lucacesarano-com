fccee5f27688e07b63fd4bd355cd252b6832cc2a
lu.cesarano <luca.cesarano1@gmail.com>
2026-05-09 22:48:52 +0000

    Add Firebase Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/app/app'
+import { initializeFirebaseAnalytics } from './firebase'
+
+initializeFirebaseAnalytics()
 
 const container = document.getElementById('root')
 const root = createRoot(container)
