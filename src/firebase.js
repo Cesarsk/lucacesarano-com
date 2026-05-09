6f1ee6bd2b009ae33322f26d429fcf732864cdd1
Luca Cesarano <luca.cesarano1@gmail.com>
2026-05-10 00:56:27 +0200

    Add Firebase Analytics (#12)
    * Add book ratings to curated reading list
    
    * Add Firebase Analytics (#11)

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics, isSupported } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQLr4LCjEVQt0SxlfzhMWTx5HDdhnGzY0',
+  authDomain: 'lucacesarano-com.firebaseapp.com',
+  databaseURL: 'https://lucacesarano-com.firebaseio.com',
+  projectId: 'lucacesarano-com',
+  storageBucket: 'lucacesarano-com.appspot.com',
+  messagingSenderId: '141578700855',
+  appId: '1:141578700855:web:f322d315730d668df697b2',
+  measurementId: 'G-L9N13B5HW0',
+}
+
+const analyticsHostname = 'lucacesarano.com'
+
+export const firebaseApp = initializeApp(firebaseConfig)
+
+export const initializeFirebaseAnalytics = () => {
+  if (typeof window === 'undefined') {
+    return Promise.resolve(null)
+  }
+
+  if (window.location.hostname !== analyticsHostname) {
+    return Promise.resolve(null)
+  }
+
+  return isSupported()
+    .then((supported) => (supported ? getAnalytics(firebaseApp) : null))
+    .catch(() => null)
+}
